4fbbcc17c67ee87aa765c3197786d3449f7b4898
Cristian1492 <c.vanegas@hotmail.com>
2025-04-16 15:16:48 -0500

    Agregando el readme.md

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { GlobalProvider } from './contexts/GlobalContext';
-import Dashboard from './pages/Dashboard';
+import Dashboard from './Pages/Dashboard';
 
 const App = () => (
   <GlobalProvider>
